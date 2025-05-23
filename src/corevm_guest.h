4db73e63f1b096d33645f26ded6e50b51c2c5677
Ivan Gankevich <ivan.gankevich@parity.io>
2025-05-23 13:04:31 +0200

    Audio next.

--- src/corevm_guest.h
+++ src/corevm_guest.h
@@ -11,78 +11,17 @@
 static_assert(sizeof(size_t) <= sizeof(uint64_t), "`size_t` is too large");
 static_assert(sizeof(void*) <= sizeof(uint64_t), "`void*` is too large");
 
-struct PolkaVM_Metadata_V2 {
-    uint8_t version;
-    uint32_t flags;
-    uint32_t symbol_length;
-    const char * symbol;
-    uint8_t input_regs;
-    uint8_t output_regs;
-    uint8_t has_index;
-    uint32_t index;
-} __attribute__ ((packed));
-
-#define POLKAVM_IMPORT_V2(arg_index, arg_return_ty, fn_name, ...) \
-static struct PolkaVM_Metadata_V2 POLKAVM_JOIN(fn_name, __IMPORT_METADATA) __attribute__ ((section(".polkavm_metadata"))) = { \
-    .version = 2, \
-    .flags = 0, \
-    .symbol_length = sizeof(#fn_name) - 1, \
-    .symbol = #fn_name, \
-    .input_regs = POLKAVM_COUNT_REGS(__VA_ARGS__), \
-    .output_regs = POLKAVM_COUNT_REGS(arg_return_ty), \
-    .has_index = 1, \
-    .index = arg_index \
-}; \
-static arg_return_ty __attribute__ ((naked)) fn_name(POLKAVM_IMPORT_ARGS_IMPL(__VA_ARGS__)) { \
-    __asm__( \
-        POLKAVM_IMPORT_DEF() \
-        "ret\n" \
-        : \
-        : \
-          [metadata] "i" (&POLKAVM_JOIN(fn_name, __IMPORT_METADATA)) \
-        : "memory" \
-    ); \
-}
-
-POLKAVM_IMPORT_V2(0, uint64_t, corevm_gas);
-POLKAVM_IMPORT_V2(1, uint64_t, corevm_alloc, uint64_t);
-POLKAVM_IMPORT_V2(2, void, corevm_free, uint64_t, uint64_t);
-POLKAVM_IMPORT_V2(3, uint64_t, corevm_yield_console_data, uint64_t, uint64_t, uint64_t);
-POLKAVM_IMPORT_V2(4, uint64_t, corevm_yield_video_frame_impl, uint64_t, uint64_t);
-POLKAVM_IMPORT_V2(5, void, corevm_video_mode_impl, uint64_t, uint64_t, uint64_t, uint64_t);
-
-#ifndef COREVM_PRINTF_BUFFER_LEN
-#define COREVM_PRINTF_BUFFER_LEN 4096
-#endif
-
-#define corevm_printf_impl(stream, format, ...) \
-    { \
-        char buffer[COREVM_PRINTF_BUFFER_LEN]; \
-        int n = snprintf(buffer, COREVM_PRINTF_BUFFER_LEN, format, ##__VA_ARGS__); \
-        if (n > 0) { \
-            if (n == COREVM_PRINTF_BUFFER_LEN) { \
-                n = COREVM_PRINTF_BUFFER_LEN - 1; \
-            } \
-            buffer[n] = 0; \
-            while (true) { \
-                uint64_t ret = corevm_yield_console_data(stream, (uint64_t)buffer, (uint64_t)(n + 1)); \
-                if (ret == 0) { \
-                    break; \
-                } \
-            } \
-        } \
-    }
-
-#define corevm_printf(format, ...) corevm_printf_impl(1, format, ##__VA_ARGS__)
-#define corevm_eprintf(format, ...) corevm_printf_impl(2, format, ##__VA_ARGS__)
+POLKAVM_IMPORT(uint64_t, corevm_gas);
+POLKAVM_IMPORT(uint64_t, corevm_alloc, uint64_t);
+POLKAVM_IMPORT(void, corevm_free, uint64_t, uint64_t);
+POLKAVM_IMPORT(uint64_t, corevm_yield_console_data, uint64_t, uint64_t, uint64_t);
+POLKAVM_IMPORT(uint64_t, corevm_yield_video_frame_impl, uint64_t, uint64_t);
+POLKAVM_IMPORT(void, corevm_video_mode_impl, uint64_t, uint64_t, uint64_t, uint64_t);
+POLKAVM_IMPORT(void, corevm_audio_mode_impl, uint64_t, uint64_t, uint64_t);
+POLKAVM_IMPORT(uint64_t, corevm_yield_audio_samples_impl, uint64_t, uint64_t);
 
 inline static void corevm_yield_video_frame(const void* frame, size_t frame_len) {
-    while (1) {
-        uint64_t ret = corevm_yield_video_frame_impl((uint64_t) frame, (uint64_t) frame_len);
-        if (ret == 0) {
-            break;
-        }
-    }
+    corevm_yield_video_frame_impl((uint64_t) frame, (uint64_t) frame_len);
 }
 
 enum CoreVmVideoFrameFormat {
@@ -103,6 +42,28 @@ inline static void corevm_video_mode(const struct CoreVmVideoMode* mode) {
         (uint64_t) mode->refresh_rate,
         (uint64_t) mode->format
     );
+}
+
+enum CoreVmAudioSampleFormat {
+    COREVM_AUDIO_S16LE = 1
+};
+
+struct CoreVmAudioMode {
+    uint32_t sample_rate;
+    uint8_t channels;
+    enum CoreVmAudioSampleFormat sample_format;
+};
+
+inline static void corevm_audio_mode(const struct CoreVmAudioMode* mode) {
+    corevm_audio_mode_impl(
+        (uint64_t) mode->channels,
+        (uint64_t) mode->sample_rate,
+        (uint64_t) mode->sample_format
+    );
+}
+
+inline static void corevm_yield_audio_samples(const void* frame, size_t frame_len) {
+    corevm_yield_audio_samples_impl((uint64_t) frame, (uint64_t) frame_len);
 }
 
 #endif
--- src/guest.c
+++ src/guest.c
@@ -13,6 +13,7 @@
 #include "polkavm_guest.h"
 #include "corevm_guest.h"
 #include "../libs/doomgeneric/doomgeneric/doomgeneric.h"
+#include "../libs/doomgeneric/doomgeneric/i_timer.h"
 #include "../libs/SDL/src/audio/SDL_audio_c.h"
 #include "../libs/SDL/src/audio/SDL_sysaudio.h"
 #include "miniz.h"
@@ -20,21 +21,35 @@
 POLKAVM_MIN_STACK_SIZE(16 * 4096);
 
 static long ext_stdout(long buffer, size_t length) {
+    corevm_yield_console_data(1, buffer, length);
     return length;
 }
 
-static void ext_output_audio(long buffer, size_t length) {}
-
 static char * ARGV[4] = {"./doom", "-iwad", "doom1.wad", "-timedemo"};
 
-uint64_t ext_main() {
+#define FRAMES_PER_SEC TICRATE
+
+#define SAMPLE_RATE 44100
+#define CHANNELS 2
+
+#define DIV_CEIL(x, y) (((x) + ((y) - 1)) / (y))
+#define MAX_SAMPLES_PER_FRAME DIV_CEIL(SAMPLE_RATE, FRAMES_PER_SEC)
+#define AUDIO_BUFFER_LEN (MAX_SAMPLES_PER_FRAME * sizeof(int16_t) * CHANNELS)
+
+uint64_t _pvm_start() {
     struct CoreVmVideoMode video_mode = {
         .width = DOOMGENERIC_RESX,
         .height = DOOMGENERIC_RESY,
-        .refresh_rate = 30,
+        .refresh_rate = FRAMES_PER_SEC,
         .format = COREVM_VIDEO_RGB88_INDEXED8,
     };
     corevm_video_mode(&video_mode);
+    struct CoreVmAudioMode audio_mode = {
+        .sample_rate = SAMPLE_RATE,
+        .channels = CHANNELS,
+        .sample_format = COREVM_AUDIO_S16LE,
+    };
+    corevm_audio_mode(&audio_mode);
     doomgeneric_Create(4, ARGV);
     while (1) {
         doomgeneric_Tick();
@@ -42,45 +57,7 @@ uint64_t ext_main() {
     return 0;
 }
 
-POLKAVM_EXPORT(uint64_t, ext_main);
-
-void DG_Init(void) {}
-
-void DG_DrawFrame(void) {}
-
-int DG_GetKey(int * is_pressed, unsigned char * key) {
-    return 0;
-}
-
-void DG_SetWindowTitle(const char * title) {}
-
-static uint32_t SUBSAMPLES = 0;
-
-void DG_SleepMs(uint32_t ms) {
-    SUBSAMPLES += ms;
-}
-
-uint32_t DG_GetTicksMs(void) {
-    // Make sure there's always forward progress.
-    SUBSAMPLES += 1;
-    return SUBSAMPLES;
-}
-
-void SDL_Delay(uint32_t ms) {
-    DG_SleepMs(ms);
-}
-
-Uint32 SDL_GetTicks(void) {
-    return SUBSAMPLES;
-}
-
-static SDL_bool AUDIO_Init(SDL_AudioDriverImpl *impl) {
-    return SDL_FALSE;
-}
-
-AudioBootStrap DUMMYAUDIO_bootstrap = {
-    "doom", "DOOM audio", AUDIO_Init, SDL_FALSE
-};
+POLKAVM_EXPORT(uint64_t, _pvm_start);
 
 static void flush_stdio() {
     fflush(stdout);
@@ -207,7 +184,6 @@ long __syscall3(long n, long a0, long a1, long a2) {
         }
         case SYS_lseek:
         {
-            fprintf(stderr, "sys_lseek\n");
             long * offset = 0;
             long * size = 0;
             if (a0 == FD_MIDI) {
@@ -244,7 +220,6 @@ long __syscall3(long n, long a0, long a1, long a2) {
         }
         case SYS_readv:
         {
-            fprintf(stderr, "sys_readv\n");
             long * fd_offset = 0;
             long * fd_size = 0;
             if (a0 == FD_MIDI) {
@@ -329,3 +304,132 @@ long __syscall6(long n, long a0, long a1, long a2, long a3, long a4, long a5) {
 
     return -ENOSYS;
 }
+
+static inline size_t next_power_of_2(size_t x) {
+    size_t n = 1;
+    while (n < x) {
+        n <<= 1;
+    }
+    return n;
+}
+
+static SDL_AudioDevice * AUDIO_DEVICE = NULL;
+
+static int AUDIO_OpenDevice(SDL_AudioDevice *this, const char *devname) {
+    this->spec.samples = next_power_of_2(SAMPLE_RATE / FRAMES_PER_SEC);
+    this->spec.format = AUDIO_S16;
+    this->spec.freq = SAMPLE_RATE;
+    this->spec.channels = CHANNELS;
+
+    SDL_CalculateAudioSpec(&this->spec);
+
+    printf("Opened audio device: buffer size = %lu\n", (unsigned long)this->spec.size);
+    AUDIO_DEVICE = this;
+    return 0;
+}
+
+static void AUDIO_CloseDevice(SDL_AudioDevice *this) {}
+
+static SDL_bool AUDIO_Init(SDL_AudioDriverImpl *impl)
+{
+    impl->OpenDevice = AUDIO_OpenDevice;
+    impl->CloseDevice = AUDIO_CloseDevice;
+    impl->OnlyHasDefaultOutputDevice = SDL_TRUE;
+    impl->ProvidesOwnCallbackThread = SDL_TRUE;
+    return SDL_TRUE;
+}
+
+AudioBootStrap DUMMYAUDIO_bootstrap = {
+    "doom", "DOOM audio", AUDIO_Init, SDL_FALSE
+};
+
+// This is based on SDL's Haiku backend.
+void doom_get_audio(void * stream, size_t len)
+{
+    SDL_AudioDevice * audio = AUDIO_DEVICE;
+    SDL_AudioCallback callback = audio->callbackspec.callback;
+    SDL_LockMutex(audio->mixer_lock);
+
+    if (!SDL_AtomicGet(&audio->enabled) || SDL_AtomicGet(&audio->paused)) {
+        if (audio->stream) {
+            SDL_AudioStreamClear(audio->stream);
+        }
+        SDL_memset(stream, audio->spec.silence, len);
+    } else {
+        SDL_assert(audio->spec.size == len);
+
+        if (audio->stream == NULL) {  /* no conversion necessary. */
+            callback(audio->callbackspec.userdata, (Uint8 *) stream, len);
+        } else {  /* streaming/converting */
+            const int stream_len = audio->callbackspec.size;
+            const int ilen = (int) len;
+            while (SDL_AudioStreamAvailable(audio->stream) < ilen) {
+                callback(audio->callbackspec.userdata, audio->work_buffer, stream_len);
+                if (SDL_AudioStreamPut(audio->stream, audio->work_buffer, stream_len) == -1) {
+                    SDL_AudioStreamClear(audio->stream);
+                    SDL_AtomicSet(&audio->enabled, 0);
+                    break;
+                }
+            }
+
+            const int got = SDL_AudioStreamGet(audio->stream, stream, ilen);
+            SDL_assert((got < 0) || (got == ilen));
+            if (got != ilen) {
+                SDL_memset(stream, audio->spec.silence, len);
+            }
+        }
+    }
+
+    SDL_UnlockMutex(audio->mixer_lock);
+}
+
+void DG_Init(void) {}
+
+// Current in-game time in nanoseconds.
+static size_t timestamp = 0;
+// Current frame number;
+static size_t frame_number = 0;
+static char audio_buffer[AUDIO_BUFFER_LEN];
+
+static void pump_audio();
+
+void DG_DrawFrame(void) {
+    pump_audio();
+    ++frame_number;
+    timestamp = frame_number * 1000000000UL / FRAMES_PER_SEC;
+}
+
+#pragma GCC push_options
+#pragma GCC optimize ("O3")
+
+static void pump_audio() {
+    size_t start_sample = frame_number * SAMPLE_RATE / FRAMES_PER_SEC;
+    size_t end_sample = (frame_number + 1) * SAMPLE_RATE / FRAMES_PER_SEC;
+    size_t buffer_len = (end_sample - start_sample) * sizeof(int16_t) * CHANNELS;
+    doom_get_audio(audio_buffer, buffer_len);
+    corevm_yield_audio_samples(audio_buffer, buffer_len);
+}
+
+void DG_SleepMs(uint32_t ms) {
+    timestamp += ((size_t)ms) * 1000UL;
+}
+
+uint32_t DG_GetTicksMs(void) {
+    return timestamp / 1000UL;
+}
+
+void SDL_Delay(Uint32 ms) {
+    DG_SleepMs(ms);
+}
+
+Uint32 SDL_GetTicks(void) {
+    return DG_GetTicksMs();
+}
+
+#pragma GCC pop_options
+
+int DG_GetKey(int * is_pressed, unsigned char * key) {
+    return 0;
+}
+
+void DG_SetWindowTitle(const char * title) {}
